da8450f03c7b683bf56795d61d1212952c326da8
laikas123 <laikas@gmail.com>
2023-02-23 06:30:20 -0800

    Added colorizing, executing shell commands based on user input, bug fixes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use std::env;
+use text_colorizer::*;
+use std::process::{Command, Stdio};
+use std::str;
 
+
+
+//TODO create regex that makes sure the provided directory is only letters, nums, 
+//and forward slashes
+
+//for even friendlier user experience, let the user choose what is allowed in a directory...
+//
 
 //allows for comparing enums with ==
 #[derive(PartialEq, Eq)]
@@ -10,16 +20,20 @@ enum resp_type {
 }
 
 fn main() {
+    println!("\n");
+    
+    let mut project_name = String::new();
+    let mut project_dir = String::new();
     
     //get arguments as tuple and split
     //to two vars
     let parsed_args  = get_args();
-    let project_name: String = parsed_args.0;
-    let project_dir: String = parsed_args.1;
+    project_name = parsed_args.0;
+    project_dir = parsed_args.1;
 
     let mut args_ok = String::new();    
 
-    println!("Project Name = {} and Project Dir = {} is this correct? (Y) (N)", project_name, project_dir);
+    println!("{} = {} and {} = {} is this correct? (Y) (N)", "Project Name".blue(), project_name.purple(), "Project Dir".green(), project_dir.purple());
 
     //read user input from command line
     std::io::stdin().read_line(&mut args_ok).unwrap();
@@ -27,6 +41,7 @@ fn main() {
     println!("You wrote: {}", args_ok);
 
 
+    //check if user was good with their arguments passed
     let mut resp = resp_type::unknown;
     
     if args_ok.trim() == "Y" || args_ok.trim() == "y" {
@@ -37,8 +52,11 @@ fn main() {
         resp = resp_type::unknown;
     }
 
+    //for getting new arguments from user
+    //if they aren't satisfied with current args
     let mut new_args_if_no = String::new();
 
+    //resolve no or unknown answers
     if resp == resp_type::no || resp == resp_type::unknown {
 
         while resp == resp_type::no || resp == resp_type::unknown {
@@ -48,7 +66,39 @@ fn main() {
             if resp == resp_type::no {
                 println!("Gotcha please enter a new project name followed by a space followed by directory");
                 std::io::stdin().read_line(&mut new_args_if_no).unwrap();
-                println!("Ok looks like {} is this correct (Y) (N) \n", new_args_if_no);
+
+                
+                let mut found_words = false;
+                
+                //make sure the user truly entered one word followed by a space
+                while found_words == false{
+                    let split_count = new_args_if_no.split(" ");
+                    if split_count.count() !=  2 {
+                        println!("\n");
+                        println!("{} please enter one word without spaces followed by a space, followed by another word without spaces \n", "Error".red());
+                        new_args_if_no = String::new();
+                        std::io::stdin().read_line(&mut new_args_if_no).unwrap();
+                        found_words = false;
+                    }else{
+                        let split = new_args_if_no.split(" ");
+                        let mut index = 0;
+                        for word in split{
+                            if index == 0 {
+                                project_name = word.to_string();
+                                index = index + 1;
+                            }else{
+                                project_dir = word.to_string();
+                            }
+                        }
+                        found_words = true;
+                    }
+                    
+                    
+
+                   
+                }
+
+                println!("{} = {} and {} = {} is this correct? (Y) (N)", "Project Name".blue(), project_name.purple(), "Project Dir".green(), project_dir.purple());
             }else{
                 println!("Sorry didn't catch that please enter (Y) or (N) \n");
             }
@@ -77,6 +127,53 @@ fn main() {
     }else{
         println!("is uknown");
     }
+
+
+    
+
+    // let output = if cfg!(target_os = "windows") {
+    //     Command::new("cmd")
+    //             .args(["/C", "echo hello Windows is terrible why do you use it?"])
+    //             .output()
+    //             .expect("failed to execute process")
+    // } else {
+    //     Command::new("sh")
+    //             .arg("-c")
+    //             .arg(format!("cd {} && cargo new {}", project_dir, project_name))
+    //             .output()
+    //             .expect("failed to execute process")
+    // };
+
+    // let hello = output.stdout;
+
+    
+    println!("Created package, would you like to push to a new github repo?");
+
+    let output2 = Command::new("sh")
+                .arg("-c")
+                .arg(format!("cd {}/{} && ls", project_dir, project_name))
+                .stdout(Stdio::piped())
+                .spawn()
+                .expect("failed to execute process");
+
+    let echo_out = output2.stdout.expect("Failed to open echo stdout");
+
+    let mut git_child = Command::new("echo")
+        .stdin(Stdio::from(echo_out))
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to start child process");
+
+    let output3 = git_child.wait_with_output().expect("Failed to wait on git");
+    
+    // assert_eq!(b"Oh no, a typo!\n", output.stdout.as_slice());
+
+    let s = match str::from_utf8(&output3.stdout) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    println!("{}", s);
 
 }
 
